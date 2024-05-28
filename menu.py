2ec22afa5468b0d9c177d28b18dba74b102dee9a
karturbis <vulkanera42@gmx.ch>
2024-05-28 18:34:39 +0200

    add optional argument caption and implement printing the caption

--- menu.py
+++ menu.py
@@ -1,8 +1,12 @@
-import sys
+"""This module provides a simple implementation
+of a menu, that can be used flexible."""
 
+def main(options: list, caption: str = "", iteration_zero: bool = True) -> int:
+    """The main method, which displays the menu."""
+    if iteration_zero:
+        options.append("Help")
 
-def main(options: list) -> int:
-
+    print(caption)
     for i, e in enumerate(options):
         print(f"Option {i+1} {e}")
     running: bool = True
@@ -15,4 +19,6 @@ def main(options: list) -> int:
             text: str = "Please input the number of the Option you want to pick."
             continue
         running = False
+    if option == len(options):
+        main(options, caption, False)
     return option
