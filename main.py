250613c088ee8d4d745d63a43c1ae1eea9a131c4
karturbis <vulkanera42@gmx.ch>
2024-05-28 20:52:16 +0200

    start implement get methods for the Oponent and PLayer class, so they can be used by the fight system

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from os.path import exists
 from os import walk
 import pickle
 import pansi
-import menu
+from menu import menu
+import fight
+
 
 class Options:
 
@@ -32,9 +34,10 @@ class Options:
         choosed: int = menu.main([i.replace(".save", "") for i in game_save_files], "Load Game Menu")
         main.load_game(game_save_files[choosed])
 
+
 class Oponent:
     """Class for normal Oponents, Bosses and Minibosses"""
-    creature_type: str = ""
+    species: str = ""
     health: int = None
     name: str = ""
     strength: int = None
@@ -44,9 +47,24 @@ class Oponent:
     attacks: dict = {}  # Attack name is key, value is the attack damage
     items: dict = {}  # Item name is key, value is list of item properties
 
-
     def __init__(self, health) -> None:
         self.health = health
+    
+    def get_attributes(self) -> dict:
+        attributes: dict = {
+            "species": self.species,
+            "health": self.health,
+            "name": self.name,
+            "strength": self.strength,
+            "speed": self.speed,
+            "krit_rate": self.krit_rate,
+            "miss_rate": self.miss_rate,
+            "attacks": self.attacks,
+            "items": self.items
+            }
+        return attributes
+
+
 
 class GameState:
     """The state of the whole world, not
@@ -76,6 +94,21 @@ class Player:
     def __init__(self, coordinates) -> None:
         self.coordinates = coordinates
 
+    def get_fight_attributes(self) -> dict:
+        fight_attributes: dict = {
+            "species": self.species,
+            "weight": self.weight,
+            "height": self.height,
+            "name": self.name,
+            "health": self.health,
+            "hunger": self.hunger,
+            "known_items": self.known_items,
+            "speed": self.speed,
+            "strength": self.strength,
+            "inventory": self.inventory,
+
+        }
+
 
 class Main:
 
@@ -104,7 +137,7 @@ class Main:
         game_save_files: list = next(walk("saves"), (None, None, []))[2]
         return game_save_files
 
-menu.main([
+menu([
     "New game",
     "Load game",
     "Character",
