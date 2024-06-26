c9252dee672d1827356ef14983efd1cab0406b53
--list <vulkanera42@gmx.ch>
2024-06-26 16:01:21 +0200

    add terminal_handler module

---
+++ terminal_handler.py
@@ -0,0 +1,71 @@
+"""Module, to have stats at the top of the terminal window."""
+
+from os import system
+from os import name
+from os import get_terminal_size
+
+
+class TerminalHandler:
+    """Contains all methods needed,
+    to display stats at the top of the
+    terminal window."""
+
+    def __init__(self, information_content: dict = None):
+        self.information_content: dict = information_content
+        self.terminal_content: list = []
+        print(self.gen_information_content_printable())
+
+    def new_print(self, print_content: str) -> None:
+        """Prints the input and additionaly
+        logs it into the terminal_content variable."""
+        self.terminal_content.append(print_content)
+        print(print_content)
+
+    def clear(self) -> None:
+        """Clears the screen."""
+        information_content_printable = self.gen_information_content_printable()
+        system("cls" if name == "nt" else "clear")
+        self.terminal_content = []
+        print(information_content_printable)
+
+    def gen_information_content_printable(self) -> str:
+        """Returns a printeble string of the
+        information, which is to be displayed
+        at the top of the window."""
+        information_content_printable: str = "-------------"
+        for key, item in self.information_content.items():
+            information_content_printable = f"{information_content_printable}\n{key}: {item}"
+        return f"{information_content_printable}\n-------------"
+
+    def get_terminal_content_printable(self) -> str:
+        """Returns a printable string of the
+        terminal history."""
+        terminal_content_printable: str = ""
+        for i in self.terminal_content:
+            terminal_content_printable = f"{terminal_content_printable}\n{i}"
+        return terminal_content_printable
+
+    def update_terminal(self):
+        """Updates the data, which is
+        displayed at the top of the screen."""
+        information_content_printable = self.gen_information_content_printable()
+        terminal_content_printable = self.get_terminal_content_printable()
+        system("cls" if name == "nt" else "clear")
+        print(f"{information_content_printable}{terminal_content_printable}")
+
+    def set_information(self, name: str, value: int):
+        """Set the information, which is
+        displayed at the top of the screen."""
+        self.information_content[name] = value
+        self.update_terminal()
+
+    def check_terminal_overflow(self):
+        """Checks, if the terminal has
+        enough columns for the content,
+        if not, the oldest entries in the
+        terminal history get deleted."""
+        if get_terminal_size()[1] <= (
+            len(self.information_content) + len(self.terminal_content) + 3
+        ):  # The two account for the two lines, whoch are printed for design reasons
+            self.terminal_content.pop(0)
+            self.update_terminal()
