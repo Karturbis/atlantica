1bfc2db4b721523e3f4c9efadf6e506570e18165
karturbis <vulkanera42@gmx.ch>
2024-10-17 15:46:48 +0200

    improve sql querey

--- language_handler_de.py
+++ language_handler_de.py
@@ -9,6 +9,8 @@ import sqlite3
 
 class Wort:
     word_data_path: str = "data/test_language_handler_de_data.sqlite"
+    connection = sqlite3.connect(word_data_path)
+    cursor = connection.cursor()
 
     @classmethod
     def create_word(
@@ -23,13 +25,20 @@ class Wort:
         person: str = None,
         gebrauch: str = None,
     ):
-        connection = sqlite3.connect(cls.word_data_path)
-        cursor = connection.cursor()
         if wortart in {"eigenname", "substantiv"}:
-            return Nomen.create_nomen(
-                connection, cursor, wortart, lemma, kasus, numerus, genus
+            return Nomen.create_nomen(wortart, lemma, kasus, numerus, genus)
+        if wortart in {"verb", "wortform_zu", "zusatz"}:
+            return Verb.create_verb(
+                wortart=wortart, lemma=lemma, form=form, person=person
             )
         return "ERROR IN create word"
+
+    @classmethod
+    def get_form(cls, table: str, tags: list, lemma: str):
+        command = f"SELECT form FROM {table} WHERE lemma LIKE '{lemma}' AND tags LIKE '%{tags[0]}%'"
+        for i in range(len(tags) - 1):
+            command = f"{command} AND tags LIKE '%{tags[i + 1]}%'"
+        return cls.cursor.execute(command).fetchall()
 
 
 class Nomen(Wort):
@@ -53,19 +62,19 @@ class Nomen(Wort):
     @classmethod
     def create_nomen(
         cls,
-        connection,
-        cursor,
         wortart: str,
         lemma: str,
         kasus: str = "",
         numerus: str = "",
         genus: str = "",
     ):
-        tags = f"{cls.tags_nomen["wortart"][wortart]}:{cls.tags_nomen["kasus"][kasus]}:{cls.tags_nomen["numerus"][numerus]}:{cls.tags_nomen["genus"][genus]}"
-        database_return = connection.execute(
-            f"SELECT form FROM nomen WHERE lemma='{lemma}' AND tags='{tags}'"
-        )
-        form = database_return.fetchall()[0][0]
+        tags = [
+            cls.tags_nomen["wortart"][wortart],
+            cls.tags_nomen["kasus"][kasus],
+            cls.tags_nomen["numerus"][numerus],
+            cls.tags_nomen["genus"][genus],
+        ]
+        form = super().get_form("nomen", tags, lemma)[0][0]
         return form
 
 
@@ -82,6 +91,28 @@ class Verb(Wort):
         },
         "person": {"1_person": "1", "2_person": "2", "3_person": "3"},
     }
+
+    @classmethod
+    def create_verb(
+        cls,
+        wortart: str,
+        lemma: str,
+        typ: str = None,
+        form: str = None,
+        person: str = None,
+    ):
+        tags = []
+        try:
+            tags.append(cls.tags_verben["wortart"][wortart])
+        except KeyError:
+            pass
+        try:
+            cls.tags_verben["typ"][typ],
+        except KeyError:
+        cls.tags_verben["form"][form],
+        cls.tags_verben["person"][person],
+        form = super().get_form("verben", tags, lemma)
+        return form
 
 
 class Adjektiv(Wort):
@@ -102,3 +133,4 @@ class Adjektiv(Wort):
 
 if __name__ == "__main__":
     print(Wort.create_word("substantiv", "Aal", "dativ", "singular", "maskulin"))
+    print(Wort.create_word("verb", "rückenschwimmen", form="partizip_1"))
