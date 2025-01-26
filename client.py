ebef634e370a374bd43f47c2940e2ee3d1f62d8b
karturbis <vulkanera42@gmx.ch>
2025-01-26 16:16:21 +0100

    start implementing connect_to_server() method

--- client.py
+++ client.py
@@ -8,14 +8,27 @@ class Client():
         self.__name = "NONE"
 
     def connect_to_server(self):
-        print("cts")
+        server_adress = input("Input the Server you want to join: ").split(":")
+        if len(server_adress) > 1:
+            server_port = int(server_adress[1])
+        else:
+            server_port = 27300
+        server_ip = server_adress[0]
+        connection = network_handler.init_client(server_ip, server_port)
+
+
+
     def start_server(self):
         pass
+
     def single_player(self):
         pass
-    
-    def set_name(self, name):
-        self.__name = name
+
+    def set_name(self, name=None):
+        if not name:
+            print("You need to input a name")
+        else:
+            self.__name = name
 
     def print_help(self, args=None) -> None:
         """Outprints all available commands."""
@@ -25,6 +38,7 @@ class Client():
         print()
 
 client = Client()
+network_handler = NetworkHandler()
 input_handler = InputHandler(
     client,
     {
