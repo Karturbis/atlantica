018072c6627c1ea629154b596fc5a9f69764f937
karturbis <vulkanera42@gmx.ch>
2025-01-26 18:07:55 +0100

    change CommandPacket to NetworkPacket

--- handler/__init__.py
+++ handler/__init__.py
@@ -2,4 +2,4 @@ from .terminal_handler import TerminalHandler
 from .database_handler import DatabaseHandler
 from .input_handler import InputHandler
 from .network_handler import NetworkHandler
-from .network_handler import CommandPacket
+from .network_handler import NetworkPacket
