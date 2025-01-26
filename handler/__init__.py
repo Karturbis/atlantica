9b8f0a326a33bb89271db128146a849f62e6d2ac
karturbis <vulkanera42@gmx.ch>
2025-01-26 04:07:22 +0100

    add network_handler.py to the handler package

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,3 +1,4 @@
 from .terminal_handler import TerminalHandler
 from .database_handler import DatabaseHandler
 from .input_handler import InputHandler
+from .network_handler import NetworkHandler
---
+++ handler/network_handler.py
@@ -0,0 +1,51 @@
+import socket
+
+class NetworkHandler:
+    connections: list = []
+    server_key = "SERVERKEY:qwerty"
+
+    @classmethod
+    def _get_ip(cls) -> str:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(0)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.254.254.254', 1))
+            ip_adress = s.getsockname()[0]
+        except Exception:
+            ip_adress = '127.0.0.1'
+        finally:
+            s.close()
+        return ip_adress
+
+    @classmethod
+    def init_server(cls, multiplayer: bool = False, port: int = 27300):
+
+        # set ip adresss:
+        if multiplayer:
+            server = NetworkHandler._get_ip()
+        else:
+            server: str = "127.0.0.1"
+        # initialize socket:
+        cls.aktive_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # bind socket:
+        try:
+            cls.aktive_socket.bind((server, port))
+        except socket.error as e:
+            str(e)
+        cls.aktive_socket.listen()
+        print(f"Server started on port {port}!")
+
+    @classmethod
+    def listen_for_connections(cls):
+        connection, address = cls.aktive_socket.accept()
+        print("Connected to:", address)
+        return connection
+
+    @classmethod
+    def send_data(cls, connection, data:str):
+        try:
+            connection.send(str.encode(data))
+            return connection.recv(2048).decode()
+        except socket.error as e:
+            print(e)
