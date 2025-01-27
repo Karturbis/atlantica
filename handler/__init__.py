4d3fd4436266bea37265b08cebcafa9fd8c2a3e8
karturbis <vulkanera42@gmx.ch>
2025-01-27 01:00:59 +0100

    begin to rewrite the network handler

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,5 +1,5 @@
 from .terminal_handler import TerminalHandler
 from .database_handler import DatabaseHandler
 from .input_handler import InputHandler
-from .network_handler import NetworkHandler
-from .network_handler import NetworkPacket
+from .network_handler import *
+
