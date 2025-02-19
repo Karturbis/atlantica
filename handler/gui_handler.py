7629f224e06a4e2acf0d3018c243fdf0286e599f
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-02-19 23:40:56 +0100

    add startup method

--- handler/gui_handler.py
+++ handler/gui_handler.py
@@ -11,10 +11,25 @@ class GuiHandler():
         self.__screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
         self.__clock = pygame.time.Clock()
         self.__dt = 0
-        self.__font = pygame.font.Font(None, 32)
+        self.__font = pygame.font.Font(None, 150)
 
     def startup(self):
-        pass
+        running = True
+        time = 0
+        print_str = "Starting..."
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            sliced_str = print_str[:int(time)]
+            if int(time) == len(print_str) + 1:
+                running = False
+            self.__screen.fill("black")
+            text_surface = self.__font.render(sliced_str, True, pygame.Color("green"))
+            self.__screen.blit(text_surface, (60, 350))
+            pygame.display.flip() 
+            time += self.__clock.tick(60) / 420
+
 
     def main_loop(self):
         running = True
@@ -35,34 +50,19 @@ class GuiHandler():
                         user_input = user_input[:-1]
                     else:
                         user_input += event.unicode
-                # window resize action (make a minimum window size):
-                elif event.type == pygame.VIDEORESIZE:
-                    width, height = event.size
-                    if width < 600:
-                        width = 600
-                    if height < 400:
-                        height = 400
-                    self.__screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
-            
-
-                        
             self.__screen.fill("black")
             text_surface = self.__font.render(user_input, True, pygame.Color('green'))
             self.__screen.blit(text_surface, (200, 200))
-
             pygame.draw.circle(self.__screen, "red", (50, 300), 40)
-
-            keys = pygame.key.get_pressed()
-
             # flip() the display to put your work on screen
             pygame.display.flip()
-
             # limits FPS to 60
             # dt is delta time in seconds since last frame, used for framerate-
             # independent physics.
             dt = self.__clock.tick(60) / 1000
 
 pg = GuiHandler()
+pg.startup()
 pg.main_loop()
 
 pygame.quit()
