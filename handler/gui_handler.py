5ba83583c1539b6a6994c8363aedb006ab5e3e3c
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-02-21 21:05:23 +0100

    add refresh() method, make terminal content not private, improve new_input() method

--- handler/gui_handler.py
+++ handler/gui_handler.py
@@ -1,7 +1,6 @@
 # Example file showing a circle moving on screen
 import pygame
 
-
 class GuiHandler():
 
     def __init__(self):
@@ -14,9 +13,9 @@ class GuiHandler():
         self.__screen = pygame.display.set_mode((self.__screen_width, self.__screen_height))
         self.__std_text_font = pygame.font.Font(None, 42)
         self.__clock = pygame.time.Clock()
-        self.__dt = 0
         self.__to_blit = []
-        self.__terminal_content = []
+        self.terminal_content = []
+        self.__command_history = [""]
         # define window part heights:
         title_height = self.__screen_height//10
         in_rect_height = self.__screen_height//20
@@ -50,6 +49,22 @@ class GuiHandler():
             pygame.display.flip()
             time += self.__clock.tick(60) / 420
 
+    def refresh(self):
+        # make title
+        title_font = pygame.font.Font(None, 90)
+        title = "Atlantica"
+        title_surface = title_font.render(title, True, self.__text_color)
+        title_rect = title_surface.get_rect(center=(self.__screen_width//2, 50))
+        # flip() the display to put your work on screen
+        self.__screen.fill(self.__bg_color)  # clear screen
+        self.debug_rects()  # color the screen sections
+        self.__screen.blit(self.__input, (10, self.__in_rect_top))
+        self.__screen.blits(self.__to_blit)  # blit the terminal_content to the screen
+        #self.__screen.blit(title_surface, title_rect)  # blit Title to screen
+        pygame.display.flip()
+        # limits FPS to 60
+        self.__clock.tick(60)
+
     def debug_rects(self):
         """prints the different sections
         of the window in bright colors"""
@@ -66,72 +81,74 @@ class GuiHandler():
         #pygame.draw.rect(self.__screen, out_color, self.__out_rect)
 
     def new_print(self, text):
-        self.__terminal_content.append(text)
+        self.terminal_content.append(text)
         line_height = pygame.font.Font.size(self.__std_text_font, "Tqpg")[1]
         upper_bound = self.__out_rect_top
         lower_bound = self.__in_rect_top
-        while len(self.__terminal_content)*line_height > lower_bound -upper_bound:  # check if content fits in out_rect
-            self.__terminal_content.pop(0)  # pop oldest content
+        while len(self.terminal_content)*line_height > lower_bound -upper_bound:  # check if content fits in out_rect
+            self.terminal_content.pop(0)  # pop oldest content
         # render text to a list of text_surfaces:
-        text_surfaces = [self.__std_text_font.render(i, True, self.__text_color) for i in self.__terminal_content]
+        text_surfaces = [self.__std_text_font.render(i, True, self.__text_color) for i in self.terminal_content]
         self.__to_blit = []  # reset self.__to_blit
         for index, text_surface in enumerate(reversed(text_surfaces)):
             self.__to_blit.append((text_surface, (10, lower_bound - (index+1) * line_height)))
 
     def clear(self):
-        self.__terminal_content = []
+        self.terminal_content = []
         self.__to_blit = []
 
     def new_input(self, prompt = "input>"):
         prompt = f"{prompt.strip(" ")} " # make sure promt contains exactly one space at the end
         font = self.__std_text_font
         running = True
-        user_input = prompt
-        title_font = pygame.font.Font(None, 90)
-        title = "Atlantica"
-        title_surface = title_font.render(title, True, self.__text_color)
-        title_rect = title_surface.get_rect(center=(self.__screen_width//2, 50))
+        user_input = ""
+        command_history_index = 1
         while running:
             # pygame.QUIT event means the user clicked X to close your window
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                # keyboard input:
+                # text input:
+                elif event.type == pygame.TEXTINPUT:
+                    user_input += event.text
+                # special keys input:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
-                        self.new_print(user_input[len(prompt):])
+                        try:
+                            user_input = self.__command_history[-command_history_index]
+                        except IndexError:
+                            command_history_index = 0
+                        command_history_index += 1
+                    elif event.key == pygame.K_DOWN:
+                        try:
+                            user_input = self.__command_history[-command_history_index]
+                        except IndexError:
+                            command_history_index = 0
+                        command_history_index -= 1
                     elif event.key == pygame.K_RETURN:
-                        if user_input[len(prompt):].lower() == "clear":
+                        if not user_input.strip(" ") == "":
+                            self.__command_history.append(user_input)
+                        command_history_index = 0
+                        if user_input.lower() == "clear":
                             self.clear()
-                        else:
-                            return user_input[len(prompt):]
-                        user_input = prompt
+                            user_input = ""
+                        elif user_input:
+                            return user_input
                     elif event.key == pygame.K_BACKSPACE:
                         user_input = user_input[:-1]
-                    else:
-                        user_input += event.unicode
-            self.__screen.fill(self.__bg_color)
-            self.debug_rects()
-            text_surface = font.render(user_input, True, self.__text_color)
-            self.__screen.blits(self.__to_blit)
-            self.__screen.blit(text_surface, (10, self.__in_rect_top))
-            self.__screen.blit(title_surface, title_rect)
-            # flip() the display to put your work on screen
-            pygame.display.flip()
-            # limits FPS to 60
-            # dt is delta time in seconds since last frame, used for framerate-
-            # independent physics.
-            dt = self.__clock.tick(60) / 1000
+            self.__input = font.render(f"{prompt}{user_input}", True, self.__text_color)
+            # blit user input to screen:
+            self.refresh()
 
+if __name__ == "__main__":
+    gui_handler = GuiHandler()
+    #gui_handler.startup()
+    while True:
+        text = gui_handler.new_input()
+        if text.lower() == "exit":
+            break
+        else:
+            gui_handler.new_print(text)
+            gui_handler.refresh()
 
-
-gui_handler = GuiHandler()
-#gui_handler.startup()
-while True:
-    text = gui_handler.new_input()
-    if text.lower() == "exit":
-        break
-    else:
-        gui_handler.new_print(text)
-
-pygame.quit()
+    pygame.quit()
