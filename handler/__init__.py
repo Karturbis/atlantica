96bc56d0b9256144ceabe8c598d33606251a7368
karturbis <118579678+Karturbis@users.noreply.github.com>
2025-02-27 01:08:01 +0100

    remove old, now unnecessary files

--- handler/__init__.py
+++ handler/__init__.py
@@ -1,7 +1,4 @@
-from .terminal_handler import TerminalHandlerOld
-from .terminal_handler import TerminalHandler
 from .database_handler import DatabaseHandler
-from .input_handler import InputHandler
 from .network_handler import *
 from .thread_handler import StoppableThread
 from .gui_handler import GuiHandler
