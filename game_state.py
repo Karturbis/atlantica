4783089b6ee4ad689972ca446be32cfa96b795d4
Karturbis <118579678+Karturbis@users.noreply.github.com>
2025-11-23 20:24:28 +0100

    change dunders to single underscores

--- game_state.py
+++ game_state.py
@@ -6,8 +6,8 @@ class GameState():
     # initialisation:
 
     def __init__(self):
-        self.__map: dict = {}# self.load_map()
-        self.__things: dict ={}# self.load_things()
+        self._map: dict = {}# self.load_map()
+        self._things: dict ={}# self.load_things()
 
     def load_map(self) -> dict:
         """Return a dict, where the keys
@@ -25,7 +25,7 @@ class GameState():
     # getter:
 
     def get_room_by_id(self, room_id: str):
-        return self.__map[room_id]
+        return self._map[room_id]
 
     def get_item_by_id(self, item_id: str):
-        return self.__things[item_id]
+        return self._things[item_id]
