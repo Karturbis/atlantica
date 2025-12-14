b32e535627e2026edb05ee476bf8b7dc01314de4
Karturbis <118579678+Karturbis@users.noreply.github.com>
2025-12-14 22:50:55 +0100

    Implement the new_input() and the clear_screen() methods. Refine commends and improve the testing method.

--- handler/terminal_handler.py
+++ handler/terminal_handler.py
@@ -1,11 +1,16 @@
-"""Module, to have stats at the top of the terminal window."""
+"""Divides the terminal into two parts, one for input and one for output.
+Provides methods, to interact with the splitted screen."""
 
 import curses
 import threading
 
 class TerminalHandler:
     """Handles user input and output to the
-    screen."""
+    screen.
+    IMPORTANT: Always use quit_terminal_handler(),
+    if you quit the terminal handler.
+    DO NOT just kill it! Otherwise the shell state
+    of the calling shell will be messed up."""
 
     def __init__(self):
         self._prompt: str = "$>"
@@ -18,7 +23,7 @@ class TerminalHandler:
         # get terminal dimensions:
         row_num, col_num = self._stdscr.getmaxyx()
         self._row_num = row_num  # needed for new print
-        self._output_window = curses.newwin(row_num-2, col_num, 1, 0)
+        self._output_window = curses.newwin(row_num-2, col_num, 0, 0)
         self._seperator_line = curses.newwin(1, col_num, row_num-2, 0)
         self._input_field = curses.newwin(1, col_num, row_num-1, 0)
         # clear the screens:
@@ -35,12 +40,13 @@ class TerminalHandler:
         """Shifts every line of the output scree
         one up and adds the given content at the
         bottom"""
+        # make sure content is a string:
+        content = str(content)
         with self._print_lock:
             self._terminal_content.append(content)
             self._output_window.clear()
-            # the 2 accounts for the space occupied by the
-            # input field and the seperator:
-            out_window_free_space = self._row_num -(2 + len(self._terminal_content))
+            height_avail = self._output_window.getmaxyx()[0]
+            out_window_free_space = height_avail - len(self._terminal_content)
             # delete the oldest prints, if new prints would not
             # fit on the screen otherwise:
             while out_window_free_space < 0:
@@ -54,15 +60,52 @@ class TerminalHandler:
             self._output_window.refresh()
 
     def new_input(self, prompt: str) -> str:
-        pass
+        """Blocking method, which returns the input typed
+        by the user. Takes @prompt as an argument. This is the
+        string, which will be displayed as the prompt."""
+        input_str: str = ""
+        prompt = f"{prompt} "  # add one space to the end of the prompt
+        inputing = True
+        in_field = self._input_field
+        in_field.addstr(prompt)
+        while inputing:
+            # get the keyboard input:
+            key = in_field.getch()
+            # check for special keyboard input:
+            if key == 10:  # the enter key
+                in_field.clear()
+                in_field.refresh()
+                inputing = False
+            elif key == 127:  # backspace
+                in_field.clear()
+                input_str = input_str[:-1]  # delete the last character
+                in_field.addstr(f"{prompt}{input_str}")
+            else:  # the inputed character is appended to the input string
+                input_str = f"{input_str}{chr(key)}"
+        if input_str == "clear":
+            self.clear_screen()
+            input_str = ""
+        return input_str
+
+    def quit_terminal_handler(self):
+        """Quits the curses window, so the
+        shell environment is not messed up"""
+        curses.endwin()
+
+
 
     def clear_screen(self) -> None:
-        pass
+        """Clears the output screen"""
+        self._terminal_content = []
+        self._output_window.clear()
+        self._output_window.refresh()
 
 
 # tests:
 
 if __name__ == "__main__":
     th = TerminalHandler()
-    while True:
-        pass
+    string = ""
+    while string != "quit":
+        string = th.new_input("test$>")
+        th.new_print(string)
